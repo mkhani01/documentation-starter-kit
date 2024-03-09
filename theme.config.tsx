58b457c91425117d4228c1b574a77e166b91c434
Mohammad khani <mohammadkhani722@gmail.com>
2024-03-09 15:05:20 +0330

    feat: initial rtl

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+  logo: <span>دانشنامه نادین سافت</span>,
+  direction:'rtl',
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'دانشنامه نادین سافت',
   },
 }
 
