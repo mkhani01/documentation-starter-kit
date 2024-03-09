b3c6681424d8eca21de1d4bf87fbe6b2ab4320ac
Mohammad khani <mohammadkhani722@gmail.com>
2024-03-10 00:38:40 +0330

    feat: latest changes added

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>دانشنامه نادین سافت</span>,
+  logo: <img src="https://nadinsoft.com/wp-content/themes/nadin/img/general-logo.png"  alt="logo" width={300} height={150}/>,
   direction:'rtl',
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'دانشنامه نادین سافت',
   },
+  head: (
+      <link rel="icon" type="image/x-icon" href="https://nadinsoft.com/wp-content/uploads/2021/07/fav.png"/>
+  )
 }
 
 export default config
